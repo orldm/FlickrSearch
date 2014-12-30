f8bd2be525131de86f6340dd208a8ebafd193be6
Dmitri <orldm@mail.ru>
2014-12-30 22:35:16 +0200

    add ordered dictionary struct

---
+++ FlickrSearch/OrderedDictionary.swift
@@ -0,0 +1,13 @@
+//
+//  OrderedDictionary.swift
+//  FlickrSearch
+//
+//  Created by Dmitri Orlov on 12/30/14.
+//  Copyright (c) 2014 Razeware. All rights reserved.
+//
+
+import Foundation
+
+struct OrderedDictionary<KeyType, ValueType> {
+    
+}
